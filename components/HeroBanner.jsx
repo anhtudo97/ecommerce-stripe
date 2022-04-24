93adcf579df5ca657132806de9ea718563fe7a5b
Zhao Yang <anhtudo97@gmail.com>
2022-04-24 13:57:34 +0700

    feat(update): banner

--- components/HeroBanner.jsx
+++ components/HeroBanner.jsx
@@ -1,7 +1,33 @@
 import React from "react";
+import Link from "next/link";
+import Image from "next/image";
 
-const HeroBanner = () => {
-  return <div>HeroBanner</div>;
+import { urlFor } from "../libs/client";
+
+const HeroBanner = ({ heroBanner }) => {
+  return (
+    <div className="hero-banner-container">
+      <div>
+        <p className="beats-solo">{heroBanner.smallText}</p>
+        <h3>{heroBanner.midText}</h3>
+        <h1>{largeText1}</h1>
+        <Image
+          src={urlFor(heroBanner.image)}
+          alt="headphones"
+          className="hero-banner-iamge"
+        />
+        <div>
+          <Link href={`/product/${heroBanner.product}`} passHref>
+            <button type="button">{heroBanner.buttonText}</button>
+          </Link>
+          <div className="desc">
+            <h5>Description</h5>
+            <p>{heroBanner.desc}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default HeroBanner;
