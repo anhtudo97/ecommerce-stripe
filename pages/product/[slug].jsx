68d7545fb033d95c7fe4d12b572420dd8aa95ddd
Zhao Yang <anhtudo97@gmail.com>
2022-04-27 23:13:11 +0700

    feat(update): context and components

---
+++ pages/product/[slug].jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const ProductDetail = () => {
+  return <div>ProductDetail</div>;
+};
+
+export const getStaticProps = async ({ params: { slug } }) => {
+  const query = `*[_type == "product" && slug.current == '${slug}'][0]`;
+  const productsQuery = '*[_type == "product"]';
+
+  const product = await client.fetch(query);
+  const products = await client.fetch(productsQuery);
+
+  console.log(product);
+
+  return {
+    props: { products, product },
+  };
+};
+
+export default ProductDetail;
